514aae84c17fce807bdf47e79c06c56b56b06560
Lamija123 <155556791+Lamija123@users.noreply.github.com>
2025-07-01 09:00:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdowns = document.getElementsByClassName("dropdown-btn");
+    for (let i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
+    }
+});
